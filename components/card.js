d415095abf0615bfc0434744dc9eb729061c2cfc
Mohamed Amine Azirgui <69221403+maaaz5@users.noreply.github.com>
2022-10-04 22:56:06 +0100

    fake db added

---
+++ components/card.js
@@ -0,0 +1,4 @@
+import styles from "../styles/Card.module.css";
+export default function card() {
+  return <div>card</div>;
+}
---
+++ data/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    branch: "Informatique",
+    questions: [
+      {
+        q: "what's the best prgramming language ?",
+        a: "JavaScript",
+      },
+      {
+        q: "what's web 3.0 ?",
+        a: "CRUD",
+      },
+      {
+        q: "which OS is better for developers ?",
+        a: "MAC",
+      },
+    ],
+  },
+];
