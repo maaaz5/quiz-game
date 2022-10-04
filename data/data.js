10570a60a4f88e924acfe60ada9fbd1eb685c387
Mohamed Amine Azirgui <69221403+maaaz5@users.noreply.github.com>
2022-10-04 23:26:49 +0100

    app logic added

--- data/data.js
+++ data/data.js
@@ -1,5 +1,6 @@
-const data = [
+export const data = [
   {
+    id: 0,
     branch: "Informatique",
     questions: [
       {
@@ -13,6 +14,24 @@ const data = [
       {
         q: "which OS is better for developers ?",
         a: "MAC",
+      },
+    ],
+  },
+  {
+    id: 1,
+    branch: "Sport",
+    questions: [
+      {
+        q: "who's the best football player in the world ?",
+        a: "Siuuu",
+      },
+      {
+        q: "what player scored most goals in one single year ?",
+        a: "ankara messi",
+      },
+      {
+        q: "best bicycle goal ?",
+        a: "semi finals JUV vs RMA, and the rest is history",
       },
     ],
   },
